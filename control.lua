70a4eaac92a243b2c4b1c63f9901397cd1919f9b
calcwizard <49937136+calcwizard@users.noreply.github.com>
2020-05-21 06:41:30 -0400

    bugfixes
    improved flare removal logic
    added range visualization using radius_visualisation_specification

--- control.lua
+++ control.lua
@@ -1,26 +1,43 @@
 
 const = {
 	names = {
-		flare_prefix = "thrower-flare-",
+		flare_name = "landmine-thrower-flare",
 		flare_type = "artillery-flare"
 	}
 }
 
-function place_flare_on_ghost(event)
-	if event.created_entity.type == "entity-ghost" and event.created_entity.ghost_type == "land-mine" then
-		event.created_entity.surface.create_entity{name=const.names.flare_prefix..event.created_entity.ghost_name,position=event.created_entity.position,force=event.created_entity.force,frame_speed=0,vertical_speed=0,height=0,movement={0,0}}
-	end
+function place_flare_on_ghost(ghost)
+	ghost.surface.create_entity{name=const.names.flare_name,position=ghost.position,force=ghost.force,frame_speed=0,vertical_speed=0,height=0,movement={0,0}}
 end
 
 function remove_flare_from_ghost(ghost)
-	local flare = ghost.surface.find_entities_filtered{type=const.names.flare_type,position=ghost.position}[1]
-	if flare then
-		flare.destroy()
-	else
-		log("couldn't find a flare when removing a land mine ghost")
+	if ghost and ghost.valid then
+		local surface = ghost.surface
+		local position = ghost.position
+		local flare = surface.find_entity(const.names.flare_name,position)
+		if flare then
+			flare.destroy()
+		end
+		for _,flare in pairs(surface.find_entities_filtered{type=const.names.flare_type,position=position,radius=2}) do
+			if surface.count_entities_filtered{position=flare.position,type="entity-ghost",ghost_type="land-mine"} == 0 then
+				flare.destroy()
+			end
+		end
 	end
 end
 
-script.on_event(defines.events.on_built_entity,place_flare_on_ghost,{{filter="ghost_type",type="land-mine"}})
+function built_entity(event)
+	local entity = event.created_entity
+	if entity and entity.valid then
+		if entity.type == "land-mine" then
+			remove_flare_from_ghost(entity)
+		elseif entity.type == "entity-ghost" and entity.ghost_type == "land-mine" then
+			place_flare_on_ghost(entity)		
+		end	
+	end
+end
+
+script.on_event(defines.events.on_built_entity,built_entity,{{filter="ghost_type",type="land-mine"},{filter="type",type="land-mine"}})
 script.on_event(defines.events.on_pre_ghost_deconstructed,function(event) remove_flare_from_ghost(event.ghost) end,{{filter="ghost_type",type="land-mine"}})
 script.on_event(defines.events.on_player_mined_entity,function(event) remove_flare_from_ghost(event.entity) end,{{filter="ghost_type",type="land-mine"}})
+script.on_event(defines.events.on_robot_built_entity,built_entity,{{filter="type",type="land-mine"}})
